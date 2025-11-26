f97a677ba75742e6edaf8c78cd1ceeb80a9d71f0
Victoria <z_ve@161.ru>
2025-11-26 10:24:22 +0300

    сервер

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"github.com/gofiber/fiber/v2"
 	"fmt"
 )
 
 func main(){
+	app := fiber.New()
+	app.Get("/", func (c *fiber.Ctx)  error{
+		return c.SendString("Hi")
+	})
 	fmt.Println("Start")
+	app.Listen("localhost:3031")
 }
