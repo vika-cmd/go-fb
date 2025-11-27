b0f8d74c4a759c52236147390857d9d3480fec41
Victoria <z_ve@161.ru>
2025-11-27 10:36:34 +0300

    router.Group

--- internal/home/handler_home.go
+++ internal/home/handler_home.go
@@ -13,10 +13,18 @@ func NewHandlerHome(router fiber.Router) {
 	hn := &HandlerHome{
 		router: router,
 	}
-	hn.router.Get("/", hn.home)
+	//hn.router.Get("/", hn.home)
+	api := hn.router.Group("/api")
+	api.Get("/", hn.home)
+	api.Get("/error", hn.errorPage)
 }
 
 func (hn *HandlerHome) home(c *fiber.Ctx) error {
 	fmt.Println("HandlerHome#home")
 	return c.SendString("HandlerHome#home")
+}
+
+func (hn *HandlerHome) errorPage(c *fiber.Ctx) error {
+	fmt.Println("HandlerHome#errorPage")
+	return c.SendString("HandlerHome#errorPage")
 }
